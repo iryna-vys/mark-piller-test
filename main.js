169d25ee7b95e95098dd3e3cd0c5d5e23f000b8c
iryna-vys <vys.iryna@gmail.com>
2021-06-20 00:43:33 +0300

    Add JS

--- main.js
+++ main.js
@@ -1,2 +1,54 @@
 'use strict'
 
+const page = document.querySelector('.page');
+
+const handleSelectLigth = () => {
+  page.classList.add('light-theme');
+  page.classList.remove('dark-theme');
+  page.classList.remove('backendless-theme');
+}
+
+const handleSelectDark = () => {
+  page.classList.add('dark-theme');
+  page.classList.remove('light-theme');
+  page.classList.remove('backendless-theme');
+}
+
+const handleSelectBackendless = () => {
+  page.classList.add('backendless-theme');
+  page.classList.remove('light-theme');
+  page.classList.remove('dark-theme');
+}
+
+const lightThemeButton = document.querySelector('.light-theme-button');
+lightThemeButton.onclick = () => handleSelectLigth();
+
+const darkThemeButton = document.querySelector('.dark-theme-button');
+darkThemeButton.onclick = () => handleSelectDark();
+
+const backendlessThemeButton = document.querySelector('.backendless-theme-button');
+backendlessThemeButton.onclick = () => handleSelectBackendless();
+
+const themeSelect = document.querySelector('.theme-select');
+themeSelect.onchange = function() {
+  switch (themeSelect.value) {
+    case 'light-theme': 
+      handleSelectLigth();
+      break;
+    case 'dark-theme':
+      handleSelectDark();
+      break;
+    case 'backendless-theme':
+      handleSelectBackendless();
+  }
+}
+
+const lightThemeRadio = document.querySelector('.light-theme-radio');
+lightThemeRadio.onchange = () => handleSelectLigth();
+
+const darkThemeRadio = document.querySelector('.dark-theme-radio');
+darkThemeRadio.onchange = () => handleSelectDark();
+
+const backendlessThemeRadio = document.querySelector('.backendless-theme-radio');
+backendlessThemeRadio.onchange = () => handleSelectBackendless();
+
