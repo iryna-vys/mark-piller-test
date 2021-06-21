70adb8df2706d255e3dca6e66aec95c6d546c0fd
iryna-vys <vys.iryna@gmail.com>
2021-06-21 22:21:05 +0300

    Add animation, modal

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 'use strict'
 
 const page = document.querySelector('.page');
+const iconsBlack = document.querySelectorAll('.link-icon-black');
+const iconsWhite = document.querySelectorAll('.link-icon-white');
 
 const handleSelectLigth = () => {
   page.classList.add('light-theme');
   page.classList.remove('dark-theme');
   page.classList.remove('backendless-theme');
+  iconsBlack.forEach(icon => {
+    icon.classList.add('link-icon');
+    icon.classList.remove('hidden');
+  });
+  iconsWhite.forEach(icon => {
+    icon.classList.add('hidden');
+    icon.classList.remove('link-icon');
+  });
 }
 
 const handleSelectDark = () => {
   page.classList.add('dark-theme');
   page.classList.remove('light-theme');
   page.classList.remove('backendless-theme');
+  iconsWhite.forEach(icon => {
+    icon.classList.add('link-icon');
+    icon.classList.remove('hidden');
+  });
+  iconsBlack.forEach(icon => {
+    icon.classList.add('hidden');
+    icon.classList.remove('link-icon');
+  })
 }
 
 const handleSelectBackendless = () => {
@@ -51,4 +69,24 @@ darkThemeRadio.onchange = () => handleSelectDark();
 
 const backendlessThemeRadio = document.querySelector('.backendless-theme-radio');
 backendlessThemeRadio.onchange = () => handleSelectBackendless();
+
+
+const modals = document.querySelectorAll('.modal');
+const imgs = document.querySelectorAll('.gallery-img');
+const modalImgs = document.querySelectorAll('.modal-content');
+const captionTexts = document.querySelectorAll('.modal-caption');
+imgs.forEach((img, i) => {
+  img.onclick = function(){
+    modals[i].style.display = 'block';
+    modalImgs[i].src = this.src;
+    captionTexts[i].innerHTML = this.alt;
+  }
+});
+
+const spans = document.querySelectorAll('.close');
+spans.forEach((span, i) => {
+  span.onclick = function() {
+    modals[i].style.display = 'none';
+  }
+});
 
